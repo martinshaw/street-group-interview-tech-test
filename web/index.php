84ccc1be8bd6187496ea0d4aabc72b36946b898c
Martin Shaw <hello@martinshaw.co>
2024-03-07 08:10:06 +0000

    Basic web tool using example data

--- web/index.php
+++ web/index.php
@@ -4,8 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homeowner CSV Upload - Street Group</title>
+
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    adsadsads
+    <form action="results.php" method="post" enctype="multipart/form-data">
+        <h1>Homeowner CSV Upload</h1>
+        <p>Please upload a CSV file containing homeowner data</p>
+        <input type="file" name="file" id="file" accept=".csv">
+        <button type="submit">Upload</button>
+    </form>
 </body>
 </html>
--- web/results.php
+++ web/results.php
@@ -1,14 +1,45 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Martinshaw\StreetGroupInterviewTechTest\HomeownerNameCsvLoader;
+
+$loader = new HomeownerNameCsvLoader();
+
+$loader->load(__DIR__ . '/../data/examples-4-.csv');
+
+$successfulNameRows = $loader->getSuccessfulNameRows();
+$failedNameRows = $loader->getFailedNameRows();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Results - Homeowner CSV Upload - Street Group</title>
+    <title>Result - Homeowner CSV Upload - Street Group</title>
+
+    <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
-    adsadsads 2
+    <table>
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Initials</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($successfulNameRows as $row): ?>
+                <tr>
+                    <td><?= $row->getTitle() ?></td>
+                    <td><?= $row->getInitial() ?></td>
+                    <td><?= $row->getFirstName() ?></td>
+                    <td><?= $row->getLastName() ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </body>
-
 </html>
