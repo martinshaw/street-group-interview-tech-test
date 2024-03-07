564a60e8ce7716dac36d3b8a6b32a23c94922a3b
Martin Shaw <hello@martinshaw.co>
2024-03-07 08:04:58 +0000

    Add boilerplate for web uploading

---
+++ web/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homeowner CSV Upload - Street Group</title>
+</head>
+<body>
+    adsadsads
+</body>
+</html>
---
+++ web/results.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Results - Homeowner CSV Upload - Street Group</title>
+</head>
+
+<body>
+    adsadsads 2
+</body>
+
+</html>
